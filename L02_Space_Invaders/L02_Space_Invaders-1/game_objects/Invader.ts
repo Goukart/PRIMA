70ff06895f3ed4049c24578d1db1119477c7feeb
Goukart <benjaminherzberger@web.de>
2021-04-09 19:49:14 +0200

    update look and generation of objects

--- L02_Space_Invaders/L02_Space_Invaders-1/game_objects/Invader.ts
+++ L02_Space_Invaders/L02_Space_Invaders-1/game_objects/Invader.ts
@@ -28,7 +28,7 @@ namespace L02_Space_Invaders_v1 {
 
     export abstract class Invader extends GameObject {
         protected readonly invader: GameObject;
-        protected readonly texture: ƒ.Node = new ƒ.Node("InvaderTexture");
+        protected readonly facade: ƒ.Node = new ƒ.Node("InvaderFacade");
         protected readonly scale: ƒ.Vector3 = ƒ.Vector3.ONE();
 
 
@@ -38,70 +38,41 @@ namespace L02_Space_Invaders_v1 {
             // But we do want to define the scale variable to scale the texture and hit box
             this.scale = _scale;
 
-            const mesh: ƒ.Mesh = new ƒ.MeshCube("CubeMesh");
+            const mesh: ƒ.Mesh = new ƒ.MeshSprite("SpriteMesh");
             this.applyMaterial(mesh, _material);
         }
 
         protected applyMaterial(_mesh: ƒ.Mesh, _material: ƒ.Material): void {
             // Make texture scalable
-            this.texture.addComponent(new ƒ.ComponentTransform);
+            this.facade.addComponent(new ƒ.ComponentTransform);
             // Scale texture
-            this.texture.mtxLocal.scale(this.scale);
+            this.facade.mtxLocal.scale(this.scale);
 
             // Attach the mesh as a component to the node
-            this.texture.addComponent(new ƒ.ComponentMesh(_mesh));
+            this.facade.addComponent(new ƒ.ComponentMesh(_mesh));
             // Create a component to attach the material via this to the node
             let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(_material);
             // Now link the material to the node
-            this.texture.addComponent(cmpMaterial);
+            this.facade.addComponent(cmpMaterial);
 
             // Add the material to the node as a separate node
-            this.addChild(this.texture);
+            this.addChild(this.facade);
         }
     }
 
     export class Squid extends Invader {
         protected static readonly scale: ƒ.Vector3 = new ƒ.Vector3(8, 8, 1);
-        protected readonly texture: ƒ.Node = new ƒ.Node("SquidInvaderTexture");
+        protected readonly facade: ƒ.Node = new ƒ.Node("SquidInvaderTexture");
 
 
         constructor(_position: ƒ.Vector3) {
             super("SquidInvader", _position, Squid.scale, squidMaterial);
-
-            //this.applyMaterial(squidMaterial);
-
-            //this.applyMaterial(squidMaterial);
-
-
-            //this.texture.name = "SquidInvaderTexture";
-            //this.texture.mtxLocal.scale(this.scale);
-
-            //this.texture.addComponent(new ƒ.ComponentMesh(new ƒ.MeshCube("CubeMesh")));
-            //this.texture.getComponent(ƒ.ComponentMesh).mtxPivot.scale(this.scale);
-
         }
-
-        /*protected applyMaterial(_material: ƒ.Material): void {
-            // Define a mesh
-            let mesh: ƒ.Mesh = new ƒ.MeshCube("CubeMesh");
-
-            // Attach the mesh as a component to the node
-            this.texture.addComponent(new ƒ.ComponentMesh(mesh));
-
-            // Create a component to attach the material via this to the node
-            let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(_material);
-
-            // Now link the material to the node
-            this.texture.addComponent(cmpMaterial);
-
-            // Add the material to the node as a separate node
-            this.addChild(this.texture);
-        }*/
     }
 
     export class Crab extends Invader {
         protected static readonly scale: ƒ.Vector3 = new ƒ.Vector3(11, 8, 1);
-        protected readonly texture: ƒ.Node = new ƒ.Node("CrabInvaderTexture");
+        protected readonly facade: ƒ.Node = new ƒ.Node("CrabInvaderTexture");
 
 
         constructor(_position: ƒ.Vector3) {
@@ -111,7 +82,7 @@ namespace L02_Space_Invaders_v1 {
 
     export class Octopus extends Invader {
         protected static readonly scale: ƒ.Vector3 = new ƒ.Vector3(12, 8, 1);
-        protected readonly texture: ƒ.Node = new ƒ.Node("SquidInvaderTexture");
+        protected readonly facade: ƒ.Node = new ƒ.Node("SquidInvaderTexture");
 
 
         constructor(_position: ƒ.Vector3) {
